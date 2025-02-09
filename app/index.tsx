a8ebd043d85bca58a2d6e2d6ad7dd6d89d394b57
Manish Tomer <mt0578902@gmail.com>
2025-02-09 08:56:36 +0530

    setup

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+import "../global.css";
+const index = () => {
+  return (
+    <View className="items-center bg-orange-400 justify-center h-full">
+      <Text className="text-4xl">MAIN PAGE</Text>
+    </View>
+  );
+};
+
+export default index;
